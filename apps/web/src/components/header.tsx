09d676eed5c188395d780bff3ba936438e4a6fb9
Namann-14 <namannayak.16@gmail.com>
2026-07-23 14:55:25 +0530

    feat: integrate Clerk authentication with providers, middleware, and header UI components

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,13 +2,27 @@
 import Link from "next/link";
 
 import { ModeToggle } from "./mode-toggle";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 
 export default function Header() {
   const links = [{ to: "/", label: "Home" }] as const;
 
   return (
     <div>
-      <div className="flex flex-row items-center justify-between px-2 py-1">
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+      {/* <div className="flex flex-row items-center justify-between px-2 py-1">
         <nav className="flex gap-4 text-lg">
           {links.map(({ to, label }) => {
             return (
@@ -21,8 +35,7 @@ export default function Header() {
         <div className="flex items-center gap-2">
           <ModeToggle />
         </div>
-      </div>
-      <hr />
+      </div> */}
     </div>
   );
 }
--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -3,12 +3,15 @@
 import { Toaster } from "@settle/ui/components/sonner";
 
 import { ThemeProvider } from "./theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
+    <ClerkProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+        <Toaster richColors />
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
